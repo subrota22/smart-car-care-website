d9bbe7456a6e864a17a869ff944b49c21c0c978c
subrota <subrota45278@gmail.com>
2023-06-27 21:41:56 +0600

    add files

---
+++ client-side/src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div className='w-96'>
+            <img src="https://i.ibb.co/df5Rc3k/page-not-foud.png" alt="error page" className='max-w-screen-lg mt-5 h-screen '/>
+        </div>
+    );
+};
+
+export default PageNotFound;
